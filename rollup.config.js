282132908effa0d1da4c464b17f1c638041cb67b
ee2dev <ee2dev@gmail.com>
2020-03-02 01:15:52 +0100

    fixed rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,20 @@
 import babel from "rollup-plugin-babel";
-
-var globals = { 
-  "d3": "d3",
-};
+import * as meta from "./package.json";
 
 export default {
-  entry: "index.js",
-  dest: "build/d3-foo.js",
-  format: "umd",
-  moduleName: "d3",
-  external: Object.keys(globals),
-  globals: globals,
-  plugins: [
-    babel({
-      exclude: "node_modules/**"})
-  ]
+  input: "index.js",
+  external: ["d3"],
+  output: {
+    file: `build/${meta.name}.js`,
+    name: "d3",
+    format: "umd",
+    indent: false,
+    extend: true,
+    // banner: `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author}`,
+    globals: {d3: "d3"},
+    plugins: [
+      babel({
+        exclude: "node_modules/**"})
+    ]
+  },
 };
